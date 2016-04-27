349d151c55417f59d0b071ae0f0afbe030a7ef11
X877 <X877@users.noreply.github.com>
2016-04-27 13:27:42 +1000

    Update Board.java
    Created getter and settter

--- src/Board.java
+++ src/Board.java
@@ -20,5 +20,22 @@ public class Board {
         }
 
     }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public void setWidth(int width) {
+        this.width = width;
+    }
+        
 
 }
