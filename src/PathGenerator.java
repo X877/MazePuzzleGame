b80867cdaaa6626c5b3fa231171f5a8cd1d19e34
grundo <grundoeat@gmail.com>
2016-05-10 22:30:55 +1000

    finished code for PathGenerator class

--- src/PathGenerator.java
+++ src/PathGenerator.java
@@ -1,7 +1,47 @@
-public class PathGenerator {
+import java.util.Random;
 
-	public void genMaze(Board board){
-				
+public class PathGenerator {
+	
+	boolean[][] seen;
+	int width;
+	int height;
+	Board board;
+
+	private boolean isInBound(int curX, int curY){
+		return (curX >= 0 && curY >= 0 && curX < width && curY < height);
+	}
+	
+	private void dfs(int curX, int curY){
+		seen[curX][curY] = true;
+		int[] dirX = new int[]{0, 1, 0, -1};
+		int[] dirY = new int[]{1, 0, -1, 0};
+		
+		for(int dir = 0; dir < Tiles.NUMWALLS; dir++){
+			int newX = curX + dirX[dir];
+			int newY = curY + dirY[dir];
+			if(isInBound(newX, newY)){
+				if(!seen[newX][newY]){
+					board.getTile(curX, curY).setWall(dir, false);
+					//The weird mod thing gives the opposite direction.
+					board.getTile(newX, newY).setWall((dir+2)%Tiles.NUMWALLS, false);
+					dfs(newX, newY);
+				}
+			}
+		}
+	}
+	
+	public PathGenerator(Board startBoard){
+		board = startBoard;
+		width = board.getWidth();
+		height = board.getHeight;
+		seen = new boolean[width][height];
+	}
+	
+	public void genMaze(){		
+		Random rand;
+		int startX = rand.nextInt(width);
+		int startY = rand.nextInt(height);
+		dfs(startX, startY);
 	}
 
 }
