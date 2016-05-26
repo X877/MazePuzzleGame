efa16c6724033bcc5aa4d4595ec2e499de1ba1c4
richietrang <richietrang@users.noreply.github.com>
2016-05-26 12:03:03 +1000

    level generator has a jframe
    The way this works must be changed in order to switch to a new level INSIDE the gui

--- src/LevelGenerator.java
+++ src/LevelGenerator.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 /**
  * Class to generate a maze board depending
  * on the difficulties
@@ -22,7 +24,7 @@ public class LevelGenerator {
      * Method to create board GUI based on difficulty
      * @return generated level GUI
      */
-    public GUI levelGen() {
+    public GUI levelGen(final JFrame frame) {
         PathGenerator path;
         GUI maze;
         switch (difficulty) {
@@ -30,19 +32,19 @@ public class LevelGenerator {
                 Board easyBoard = new Board(15, 25);
                 path = new PathGenerator(easyBoard);
                 path.genMaze();
-                maze = new GUI(easyBoard, 25, 600, 290, 1);
+                maze = new GUI(easyBoard, 25, 600, 290, 1, frame);
                 return maze;
             case 2:
                 Board medBoard = new Board(25, 30);
                 path = new PathGenerator(medBoard);
                 path.genMaze();
-                maze = new GUI(medBoard, 25, 690, 250, 2);
+                maze = new GUI(medBoard, 25, 690, 250, 2, frame);
                 return maze;
             case 3:
                 Board hardBoard = new Board(33, 45);
                 path = new PathGenerator(hardBoard);
                 path.genMaze();
-                maze = new GUI(hardBoard, 20, 715, 185, 3);
+                maze = new GUI(hardBoard, 20, 715, 185, 3, frame);
                 return maze;
 
         }
