148cd7c46a0f88ba300b22cd27f224c47c8c52f3
Nicholas Yuwono <alpha.x877@gmail.com>
2016-05-27 18:44:49 +1000

    Added hint/WAM BOOSTER button for toggling hint. Changed timer looks

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.util.Stack;
@@ -24,6 +21,7 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 	private static final int ticksPerImageRotation = 8;
 	private int subImageRotation = 0;
 	private int imageRotation = 0;
+    private int toggleHint = 0;
     private String level;
     private String count;
 	private Image[] leftPlayerImages;
@@ -35,9 +33,10 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 	
 	private Stack<Character> keysPressed;
 	private Game game;
-    private Timer actionTimer;
+    private Timer tickTimer;
     private JFrame exitMainMenu;
 
+    private JToggleButton hint;
     private JLabel timerLbl;
     private JButton btnBackToMenu;
     private JLabel fadeLoseLbl;
@@ -57,8 +56,8 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         this.posFromLeft = posFromLeft;
         this.addKeyListener(this);
         this.setFocusable(true);
-        this.actionTimer = new Timer(tickTime, this);
-        this.actionTimer.start();
+        this.tickTimer = new Timer(tickTime, this);
+        this.tickTimer.start();
 
         this.upPlayerImages = new Image[2];
         this.downPlayerImages = new Image[2];
@@ -66,16 +65,13 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         this.rightPlayerImages = new Image[2];
         this.stayingPlayerImages = new Image[1];
         this.endTime = 0;
-        addBackToMenuBtn(frame);
         this.frame = frame;
-        
         this.game = new Game(mazeBoard,(double)16/this.tileSize);
 
         keysPressed = new Stack<Character>();
         
         this.subImageRotation = 0;
         this.imageRotation = 0;
-        addTimerLbl();
 
         if (difficulty == 1) {
             this.level = "Easy";
@@ -89,6 +85,10 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         this.img = new ImageIcon(this.getClass().getResource("/images/" + level + "/background.png")).getImage();
 
         this.count = "";
+
+        addTimerLbl();
+        addHintBtn();
+        addBackToMenuBtn(frame);
     }
 
     /**
@@ -130,6 +130,11 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
                 if (!currTile.isStartPoint() && !currTile.isEndPoint()) {
                     g2d.drawImage(currTile.getTile(), x1, y1, null);
 
+                    if (currTile.getHint() && toggleHint == 1) {
+                        tileToDraw = "Star";
+                        currTile.setTile(new ImageIcon(this.getClass().getResource("/images/" + level + "/PickUps/" + tileToDraw + ".png")).getImage());
+                        g2d.drawImage(currTile.getTile(), powerUpX, powerUpY, null);
+                    }
                     if (currTile.getState() != 0) {
                         if (currTile.getState() == Tiles.BEER) {
                             tileToDraw = "Beer";
@@ -161,12 +166,12 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 
     /**
      * Method to draw the player
-     * @param g
+     * @param g Graphics to draw the player on
      */
     public void drawPlayer(Graphics g) {
 
+    	// Loads all the player sprites
         Graphics2D g2d = (Graphics2D) g;
-
 
         upPlayerImages[0] = new ImageIcon(this.getClass().getResource("/images/" + level + "/Player/Player_Backwards_1.png")).getImage();
         upPlayerImages[1] = new ImageIcon(this.getClass().getResource("/images/" + level + "/Player/Player_Backwards_2.png")).getImage();
@@ -183,12 +188,13 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         stayingPlayerImages[0] = new ImageIcon(this.getClass().getResource("/images/" + level + "/Player/Player_Still.png")).getImage();
 
         Player player = game.getPlayer();
+        // Finds the coordinate to draw the player at
         int playerSize = (int) (tileSize*game.getPlayerSize()+1);
         int playerDisplayX =(int)(posFromLeft+player.getX()*tileSize)+1;
         int playerDisplayY = (int)(posFromBottom-player.getY()*tileSize-tileSize+2-playerSize);
-        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        
         Image img[] = null;
-        //g2d.fillRect(playerDisplayX, playerDisplayY, playerSize, playerSize);
+        // Decides which image set to display based on player diretion
         if (player.getDirection() == Player.UP){
             img  = upPlayerImages;
         }else if (player.getDirection() == Player.DOWN){
@@ -202,7 +208,7 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         }
 
         subImageRotation++;
-
+        // Every certain amount of ticks, change the player sprite to the next one in the set
         if (subImageRotation == GUI.ticksPerImageRotation){
             this.imageRotation = (this.imageRotation+1)%img.length;
             subImageRotation = 0;
@@ -211,18 +217,20 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         if (img.length == 1){
             imageRotation = 0;
         }
-
+        
+        // Draw the image
         g2d.drawImage(img[imageRotation], playerDisplayX, playerDisplayY, null);
     }
 
     /**
      * Method to draw fog around the player
-     * @param g
+     * @param g Graphics to draw around
      */
-    public void drawBoardFog(Graphics g) {
+    public void drawTransition(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
-
+        
+        // Find the centre of the fog to draw upon
         Player player = game.getPlayer();
         int playerSize = (int) (tileSize*game.getPlayerSize()+1);
         int playerDisplayX =(int)(posFromLeft+player.getX()*tileSize)+1;
@@ -231,18 +239,20 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         int xCentre = playerDisplayX + playerSize/2;
         int yCentre = playerDisplayY + playerSize/2;
 
-        if (game.getState() == game.LOST) {
+        // Move this code, it shouldn't be here. 
+        // Probably place in GUI.tick or in a seperate function called say gameCheckFinished
+        if (game.getState() == Game.LOST) {
             //Change the timer text
             this.timerLbl.setText("TIME IS UP!!!");
             //Store the end time once only
             if (Math.abs(game.getTime()) > 0 && this.endTime == 0){
                 this.endTime = Math.abs(game.getTime());
             }
-            System.out.println(this.endTime + " " + Math.abs(game.getTime()));
+            //System.out.println(this.endTime + " " + Math.abs(game.getTime()));
             //Game waits 450ms for the fog to cover the screen before going to the lose screen
             if(Math.abs(game.getTime())- this.endTime >= 650){
             	g.clearRect(0, 0, 9999, 9999);
-                actionTimer.stop();
+                tickTimer.stop();
                 JButton exit = new JButton();
                 JLabel loseLabel = new JLabel();
                 Image loseImage  = new ImageIcon(this.getClass().getResource("/images/wallpaper3.png")).getImage();
@@ -266,26 +276,17 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
                     }
                 });
 
-    		    /*
-        	    Font end = new Font("Copperplate Gothic Light", Font.BOLD, 30);
-        	    g.setFont(end);
-        	    g.setColor(Color.WHITE);
-        	    g.drawString("Time has run out. You have to live among other hobos", 236, 200);
-        	   */
-                //actionTimer.stop();
             }
 
-        } else if (game.getState() == game.WON){
+        } else if (game.getState() == Game.WON){
 
 
-        } else if (game.getState() == game.PLAYING) {
+        } else if (game.getState() == Game.PLAYING) {
             
         }
         
         int diameter = game.getVisionRange() * tileSize;
         drawFog(xCentre, yCentre, diameter, g2d);
-
-
     }
 
     /**
@@ -318,38 +319,42 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
         super.paintComponent(g);
         tickTime();
         g.drawImage(img, 0, 0, this);
-        if (game.getState() == game.PAUSED){
+        if (game.getState() == Game.PAUSED){
         	drawPlayer(g);
-        	drawBoardFog(g);
+        	drawTransition(g);
         }else{
 	        drawBoard(g);
 	        drawPlayer(g);
-	        drawBoardFog(g);
+	        drawTransition(g);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
     	Player player = game.getPlayer();
-    	game.getBoard().showHint((int) player.getX(),(int) player.getY());
+    	game.getBoard().showHint((int) player.getX(), (int) player.getY());
     	
     	if (!focusRequested){
     		this.requestFocus();
     		focusRequested = true;
     	}
-    	game.tick();
-    	game.movePlayer(keysPressed);
+    	game.tick(keysPressed);
         repaint();
     }
 
+    
     @Override
+    /**
+     * Adds key pressed to key pressed list
+     */
     public void keyPressed(KeyEvent e) {
     	if (!keysPressed.contains(e.getKeyChar())){
-    		//System.out.println(ADDING" + keysPressed);
     		keysPressed.add(e.getKeyChar());
     	}
     }
-
+    /**
+     * Removes key released from key pressed list
+     */
     @Override
     public void keyReleased(KeyEvent e) {
     	keysPressed.remove((Object) e.getKeyChar());
@@ -357,10 +362,17 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 
     @Override
     public void keyTyped(KeyEvent arg0) {
-        // TODO Auto-generated method stub
     }
     
+    /**
+     * Draw fog of diameter around the coordinates
+     * @param xCentre 	X the fog centers around
+     * @param yCentre 	Y the fog centers around
+     * @param diameter 	Diameter of the fog in px
+     * @param g2d		Graphics to draw the fog
+     */
     private void drawFog(int xCentre, int yCentre, int diameter, Graphics2D g2d){
+    	// Creates a square around the playing field and subtracts the specified circle from that square
     	g2d.setColor(Color.BLACK);
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, 
         	      RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
@@ -377,18 +389,42 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
     }
     
     public void addTimerLbl(){
-		this.timerLbl = new JLabel();
-		this.timerLbl.setText(Integer.toString(game.getTime()));
-		this.timerLbl.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
-		this.timerLbl.setForeground(Color.white);
-		this.timerLbl.setBorder(BorderFactory.createLineBorder(Color.white));
-		this.timerLbl.setVisible(true);
-		this.timerLbl.setBounds(583, 100, 200, 40);
-		this.add(timerLbl);
+		timerLbl = new JLabel();
+        timerLbl.setText(Integer.toString(game.getTime()));
+        timerLbl.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 35));
+        timerLbl.setBackground(Color.ORANGE);
+        timerLbl.setOpaque(true);
+        timerLbl.setForeground(Color.BLUE);
+		timerLbl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		timerLbl.setVisible(true);
+		timerLbl.setBounds(583, 100, 200, 40);
+		add(timerLbl);
     }
 
     public void tickTime() {
-        this.timerLbl.setText(Integer.toString(game.getTime()));
+        timerLbl.setText(Integer.toString(game.getTime() / 1000));
+    }
+
+    public void addHintBtn(){
+        hint = new JToggleButton("WAM BOOSTER");
+        hint.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
+        hint.setBounds(500, 550, 200, 50);
+        add(hint);
+
+        //If the button is pressed open up the window option and pause the game
+        hint.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (focusRequested){
+                    focusRequested = false;
+                }
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    toggleHint = 1;
+                } else if (e.getStateChange() == ItemEvent.DESELECTED) {
+                    toggleHint = 0;
+                }
+            }
+        });
     }
     
     public void addBackToMenuBtn(final JFrame frame){
@@ -399,28 +435,29 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 		
         //If the button is pressed open up the window option and pause the game
 		btnBackToMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				actionTimer.stop();
-				int oldState = game.getState();
-				game.setState(Game.PAUSED);
-			    String message = "Are you sure you wish to exit to main menu?";
-			    int answer = JOptionPane.showConfirmDialog(exitMainMenu, message, message, JOptionPane.YES_NO_OPTION);
-			    if(answer == JOptionPane.YES_NO_OPTION) {
-			        MenuLabel menu = new MenuLabel(frame);
-					menu.setVisible(true);	
-					frame.getContentPane().removeAll();
-					frame.getContentPane().add(menu);
-				    frame.revalidate();
-			    } else if (answer == JOptionPane.NO_OPTION || answer == JOptionPane.CANCEL_OPTION){
-			    	game.setState(oldState);
-			    	//continue the game
-			        actionTimer.start();
-			        exitMainMenu.getContentPane().removeAll();
-					exitMainMenu.revalidate();	
-			    } 
-			}
-		});
+            public void actionPerformed(ActionEvent e) {
+                tickTimer.stop();
+                int oldState = game.getState();
+                game.setState(Game.PAUSED);
+                String message = "Are you sure you wish to exit to main menu?";
+                int answer = JOptionPane.showConfirmDialog(exitMainMenu, message, message, JOptionPane.YES_NO_OPTION);
+                if (answer == JOptionPane.YES_NO_OPTION) {
+                    MenuLabel menu = new MenuLabel(frame);
+                    menu.setVisible(true);
+                    frame.getContentPane().removeAll();
+                    frame.getContentPane().add(menu);
+                    frame.revalidate();
+                } else if (answer == JOptionPane.NO_OPTION || answer == JOptionPane.CANCEL_OPTION) {
+                    game.setState(oldState);
+                    //continue the game
+                    tickTimer.start();
+                    exitMainMenu.getContentPane().removeAll();
+                    exitMainMenu.revalidate();
+                }
+            }
+        });
 	}
+
     
     public void goBackToMenuPanel(final JFrame frame){
     	JLabel endGame = new JLabel();
@@ -437,11 +474,5 @@ public class GUI extends JPanel implements KeyListener, ActionListener{
 	    frame.revalidate();
     }
     
-    public void addLevelHighScore(){
-    	
-    }
-    public void addTotalHighScore(){
-    	
-    }
 }
 
