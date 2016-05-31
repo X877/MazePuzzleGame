ce78724a0696d5ed2128980a23682ad5039cce30
richietrang <richietrang@users.noreply.github.com>
2016-05-31 22:27:21 +1000

    Final fixes positions for buttons,labels and fixes high scores

--- src/TransitionLevelLabel.java
+++ src/TransitionLevelLabel.java
@@ -3,7 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 /**
  * Loads the transition label and refreshes the frame
@@ -17,6 +20,7 @@ public class TransitionLevelLabel extends JLabel {
 	private Image img;
 	private String level;
 	private int prevScores;
+	private int levelScore;
 	private JTextArea txtLevelScore;
 	private JTextArea txtTotalScore;
 	private JButton btnBackToMenu;
@@ -38,34 +42,57 @@ public class TransitionLevelLabel extends JLabel {
             this.level = "3";
         } else if (difficulty == 4) {
             this.level = "4";
+        } else if (difficulty == 5){
+        	this.level = "5";
         }
         //Set the background
         this.setText("");
 		this.img = new ImageIcon(this.getClass().getResource("/images/transition" +  difficulty + ".png")).getImage();
+        System.out.println("Level number is:" + level);
 		this.setIcon(new ImageIcon(img));
 		//Add the button
 		
 		this.prevScores = prevScores;
+		System.out.println(prevScores);
 		//Add the level score text
 	   	txtLevelScore = new JTextArea();
 		txtLevelScore.setText("Level Score: " + Integer.toString(levelScore));
-		txtLevelScore.setBounds(583, 200, 400, 40);
+		this.levelScore = levelScore;
+		
 		txtLevelScore.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
 		this.add(txtLevelScore);
 		
 		txtTotalScore = new JTextArea();
+		//System.out.println(levelScore +" " + prevScores);
 		int totalScore = levelScore + prevScores;
 		prevScores = totalScore;
-		txtTotalScore.setText("Total Score: " + totalScore );
-		txtTotalScore.setBounds(583, 300, 400, 40);
+		
+		txtTotalScore.setText("Total Score: " + totalScore ); //
+		
 		txtTotalScore.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
 		this.add(txtTotalScore);
-		if(difficulty != 5){
+		//System.out.println("prevScores rn is "  + prevScores);
+		if(difficulty != 6){
 		addNextLevelBtn(frame, prevScores);
-		}
-		
-		if (difficulty == 5){
-			addBackToMenuBtn(frame);
+		    if (level == "2"){
+		        txtLevelScore.setBounds(650, 650, 300, 40);
+		        txtTotalScore.setBounds(1000, 650, 300, 40);
+		        nextLevelBtn.setBounds(925, 520, 200, 50);
+
+		    } else if (level == "3"){
+		        txtLevelScore.setBounds(1000, 500, 300, 40);
+		        txtTotalScore.setBounds(1000, 600, 300, 40);
+		        nextLevelBtn.setBounds(1045, 400, 200, 50);
+		    } else if (level == "4"){
+		        txtLevelScore.setBounds(30, 400, 280, 40);
+		        txtTotalScore.setBounds(30, 500, 280, 40);
+		        nextLevelBtn.setBounds(50, 600, 200, 50);
+		    } else if (level == "5"){
+		        txtLevelScore.setBounds(560, 200, 300, 40);
+		        txtTotalScore.setBounds(560, 300, 300, 40);
+		        checkScore();
+		        addBackToMenuBtn(frame);
+		    } 
 		}
 	}
 	
@@ -77,7 +104,6 @@ public class TransitionLevelLabel extends JLabel {
     public void addNextLevelBtn(final JFrame frame, final int score){
 		nextLevelBtn = new JButton("Next Level");
 		nextLevelBtn.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
-		nextLevelBtn.setBounds(583, 550, 200, 50);
 		add(nextLevelBtn);
 		nextLevelBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -129,8 +155,9 @@ public class TransitionLevelLabel extends JLabel {
 	public void addBackToMenuBtn(final JFrame frame){
 		btnBackToMenu = new JButton("Main Menu");
 		btnBackToMenu.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 20));
-		btnBackToMenu.setBounds(583, 550, 200, 50);
-		add(btnBackToMenu);
+        btnBackToMenu.setBounds(560, 650, 300, 40);
+        add(btnBackToMenu);
+		
 		btnBackToMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				MenuLabel menu = new MenuLabel(frame);
@@ -140,5 +167,47 @@ public class TransitionLevelLabel extends JLabel {
 				frame.revalidate();
 			}
 		});
+	}
+	
+	
+	//if endTime + prevTime > checkScore()
+    public void checkScore(){
+    	String strOldScore = getHighScore();
+    	int intOldScore = Integer.parseInt((strOldScore.split(":")[1]));
+    	int newScore = levelScore + prevScores;
+    	System.out.println("Enters here once?>>????");
+    	if (levelScore + prevScores  > intOldScore){
+		    String name = JOptionPane.showInputDialog("You set a new highscore! What is your name?");
+		    if(name.length() > 0){
+	            String highScore = name + ":" + newScore;
+	            File scoreFile = new File("highscore.dat");
+	            //does not exist
+	            if(!scoreFile.exists()){
+	    	        try {
+	    	            scoreFile.createNewFile();
+	    	        } catch(IOException e){
+	        	        e.printStackTrace();
+	                }
+	            }
+
+	            FileWriter writeFile = null;
+	            BufferedWriter writer = null;
+	            try{
+	    	        writeFile = new FileWriter(scoreFile);
+	    	        writer = new BufferedWriter(writeFile);
+	    	        writer.write(highScore);
+	            } catch (Exception e){
+	    	    //errors if not found
+	            } finally {
+	    	        try{
+	    	            if (writer != null){
+	    		            writer.close();
+	    	            }
+	    	        } catch (Exception e){
+	    	        }
+	            }
+		    }
+    	}
+	        
 	}
 }
